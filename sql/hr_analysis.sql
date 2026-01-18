ab55864c34f7cd41c87deb8123d38da45e6dee8f
suraj-badoni-analytics <suradzh.bad@gmail.com>
2026-01-18 18:44:46 +0100

    hr_analysis.sql

--- sql/hr_analysis.sql
+++ sql/hr_analysis.sql
@@ -1,75 +1,94 @@
 Oracle SQL HR Analytics Project
 
 
-Task 1. Employees with department names :
-select 
-a.FIRST_NAME,
-a.LAST_NAME,
-b.DEPARTMENT_NAME
-from 
-EMPLOYEES a
-left join 
-DEPARTMENTS b on a.DEPARTMENT_ID = b.DEPARTMENT_ID;
+Task 1.  
+  Business Question: 
+   Employees with department names :
+  
+SELECT
+   a.FIRST_NAME,
+   a.LAST_NAME,
+   b.DEPARTMENT_NAME
+FROM 
+   EMPLOYEES a
+LEFT JOIN 
+   DEPARTMENTS b on a.DEPARTMENT_ID = b.DEPARTMENT_ID;
 
 
 
-Task 2. Employees without department :
-select 
-a.FIRST_NAME,
-a.LAST_NAME,
-b.DEPARTMENT_NAME
-from 
-EMPLOYEES a
-left join 
-DEPARTMENTS b on a.DEPARTMENT_ID = b.DEPARTMENT_ID
-where b.DEPARTMENT_ID is null;
+Task 2.
+  Business Question: 
+   Employees without department :
+  
+SELECT
+   a.FIRST_NAME,
+   a.LAST_NAME,
+   b.DEPARTMENT_NAME
+FROM 
+   EMPLOYEES a
+LEFT JOIN 
+   DEPARTMENTS b ON a.DEPARTMENT_ID = b.DEPARTMENT_ID
+WHERE 
+   b.DEPARTMENT_ID is null;
 
 
 
-Task 3. Employees with salary above company average :
-select
-FIRST_name,
-LAST_NAME,
-SALARY
-from
-EMPLOYEES
-where
-salary > (select avg(salary)
-from EMPLOYEES);
+Task 3. 
+  Business Question: 
+   Which employees earn above the company average salary? :
+  
+SELECT
+   FIRST_name,
+   LAST_NAME,
+   SALARY
+FROM
+   EMPLOYEES
+WHERE
+   salary > 
+   (SELECT avg(salary)
+FROM EMPLOYEES
+  );
 
 
 
-Task 4. Average salary per department (only>8000) :
-select
-a.DEPARTMENT_NAME,
-avg(b.SALARY) as avg_salary
-from 
-DEPARTMENTS A
-join
-EMPLOYEES b on a.DEPARTMENT_ID  = b.DEPARTMENT_ID
-group by 
-a.DEPARTMENT_NAME
-having
-avg(b.SALARY)>8000;
+Task 4. 
+  Business Question: 
+   Which departments have an average salary greater than 8000? 
+  
+SELECT
+   a.DEPARTMENT_NAME,
+   avg(b.SALARY) AS avg_salary
+FROM 
+   DEPARTMENTS A
+JOIN
+   EMPLOYEES b ON a.DEPARTMENT_ID  = b.DEPARTMENT_ID
+GROUP BY 
+   a.DEPARTMENT_NAME
+HAVING
+   avg(b.SALARY)>8000;
+  
 
 
 
-Task 5.Number of employees per department (including empty) :
-select
-a.DEPARTMENT_NAME,
-count (b.DEPARTMENT_ID) as employee_count
-from 
-DEPARTMENTS a
-left join
-EMPLOYEES b on a.DEPARTMENT_ID = b.DEPARTMENT_ID
-group by 
-a.DEPARTMENT_NAME;
+Task 5.
+  Business Question: 
+   How many employees work in each department, including departments with no employees? 
+  
+SELECT
+   a.DEPARTMENT_NAME,
+COUNT (b.DEPARTMENT_ID) AS employee_count
+FROM
+   DEPARTMENTS a
+LEFT JOIN
+   EMPLOYEES b on a.DEPARTMENT_ID = b.DEPARTMENT_ID
+GROUP BY
+   a.DEPARTMENT_NAME;
 
 
 
 
-Conclusions :
+Conclusion :
 
-Using Oracle HR schema, I analyzed employee distribution and salaries across departments.
+Using Oracle HR schema, I analyzed employee distribution and salaries across departments. 
 Identified departments with high average salaries and employees earning above company average.
 The project demonstrates skills in joins, aggregations, subqueries and handling NULL values.
